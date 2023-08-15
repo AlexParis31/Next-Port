91b2848f0f02542f4a7303db15fd6ead686647f8
Alexander Parisi <parisialexander@gmail.com>
2023-08-15 16:51:20 -0400

    fonts

--- components/About.js
+++ components/About.js
@@ -7,7 +7,7 @@ const Aboutt = () => {
         <div className=' text-white z-[2] bg-center bg-cover customTwo-img '>
             <div class="py-16  ">  
                 <div class="container bg-cover bg-center  p-5 mx-auto mt-8 px-6 text-gray-600 md:px-12 xl:px-6 ">
-                <h2 class="font-Montserrat text-center text-7xl xs:text-6xl text-black mt-10 mb-5" >Hi! I&apos;m Alex, </h2>
+                <h2 class="font-Montserrat text-center text-7xl xs:text-6xl text-black mt-7 mb-5" >Hi! I&apos;m Alex, </h2>
                 <h2 className='text-4xl text-center font-Montaga tracking-wide text-black mb-10 '>A passionate and versatile software engineer.</h2>
                     <div class="space-y-6 md:space-y-0 md:flex justify-center md:gap-6 lg:items-center lg:gap-12">
 
@@ -15,7 +15,7 @@ const Aboutt = () => {
                             <img src="https://i.ibb.co/syBWGsS/Screen-Shot-2023-03-01-at-5-19-11-PM-2.png" alt="image" loading="lazy"  />
                         </div>
 
-                        <div class="md:7/12 lg:w-5/12">
+                        <div class="md:7/12 lg:w-7/12">
                             <p class="mt-6 font-bold text-gray-600 md:text-2xl"> </p>
 
                             <p class="font-Gabriela mt-6 text-[#000000c6] md:text-xl">Born and raised in beautiful Italy, I later pursued my undergraduate degree in the United States, where I honed my skills and developed a deep passion for technology. My international background has taught me how to quickly adapt to new environments, embrace different perspectives, and work effectively with individuals from various backgrounds.</p>
